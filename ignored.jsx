c5a935f07f0a0196a3fdd7e94e39a8a88a668009
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-07-07 02:41:56 +0530

    add

---
+++ ignored.jsx
@@ -0,0 +1,419 @@
+//  {
+//       "info": {
+//         "id": "769955",
+//         "name": "Olio - The Wood Fired Pizzeria",
+//         "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/17/4dd96acd-c82a-427a-b1ae-22c3a5584bf5_769955.JPG",
+//         "locality": "Near Reliance Fresh",
+//         "areaName": "East Vinod Nagar",
+//         "costForTwo": "₹300 for two",
+//         "cuisines": [
+//           "Pizzas",
+//           "Pastas",
+//           "Italian",
+//           "Fast Food",
+//           "Snacks",
+//           "Beverages",
+//           "Desserts"
+//         ],
+//         "avgRating": 4.3,
+//         "parentId": "11633",
+//         "avgRatingString": "4.3",
+//         "totalRatingsString": "2.3K+",
+//         "sla": {
+//           "deliveryTime": 25,
+//           "lastMileTravel": 2.6,
+//           "serviceability": "SERVICEABLE",
+//           "slaString": "25-30 mins",
+//           "lastMileTravelString": "2.6 km",
+//           "iconType": "ICON_TYPE_EMPTY"
+//         },
+//         "availability": {
+//           "nextCloseTime": "2025-07-06 06:00:00",
+//           "opened": true
+//         },
+//         "badges": {
+//           "imageBadges": [
+//             {
+//               "imageId": "newg.png",
+//               "description": "Gourmet"
+//             }
+//           ]
+//         },
+//         "isOpen": true,
+//         "type": "F",
+//         "badgesV2": {
+//           "entityBadges": {
+//             "imageBased": {
+//               "badgeObject": [
+//                 {
+//                   "attributes": {
+//                     "description": "Gourmet",
+//                     "imageId": "newg.png"
+//                   }
+//                 }
+//               ]
+//             },
+//             "textBased": {},
+//             "textExtendedBadges": {}
+//           }
+//         },
+//         "aggregatedDiscountInfoV3": {
+//           "header": "ITEMS",
+//           "subHeader": "AT ₹69"
+//         },
+//         "differentiatedUi": {
+//           "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//           "differentiatedUiMediaDetails": {
+//             "lottie": {},
+//             "video": {}
+//           }
+//         },
+//         "reviewsSummary": {},
+//         "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//         "restaurantOfferPresentationInfo": {},
+//         "externalRatings": {
+//           "aggregatedRating": {
+//             "rating": "--"
+//           }
+//         },
+//         "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+//       },
+//       "analytics": {
+//         "context": "seo-data-f06da16e-1808-413f-8f62-6309a814cecd"
+//       },
+//       "cta": {
+//         "link": "https://www.swiggy.com/city/delhi/olio-the-wood-fired-pizzeria-near-reliance-fresh-east-vinod-nagar-rest769955",
+//         "type": "WEBLINK"
+//       }
+//     },
+
+
+//        {
+//       "info": {
+//         "id": "650503",
+//         "name": "NH1 Bowls - Highway To North",
+//         "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/8/eb71d363-d0dc-4229-9ee5-a3f4320a5977_650503.jpg",
+//         "locality": "Mayur Vihar",
+//         "areaName": "Phase1",
+//         "costForTwo": "₹250 for two",
+//         "cuisines": ["North Indian", "Punjabi", "Home Food"],
+//         "avgRating": 4.4,
+//         "parentId": "22452",
+//         "avgRatingString": "4.4",
+//         "totalRatingsString": "969",
+//         "sla": {
+//           "deliveryTime": 15,
+//           "lastMileTravel": 2.1,
+//           "serviceability": "SERVICEABLE",
+//           "slaString": "10-20 mins",
+//           "lastMileTravelString": "2.1 km",
+//           "iconType": "ICON_TYPE_EMPTY"
+//         },
+//         "availability": {
+//           "nextCloseTime": "2025-07-06 02:00:00",
+//           "opened": true
+//         },
+//         "badges": {
+//           "textExtendedBadges": [
+//             {
+//               "iconId": "guiltfree/GF_Logo_android_3x",
+//               "shortDescription": "options available",
+//               "fontColor": "#7E808C"
+//             }
+//           ]
+//         },
+//         "isOpen": true,
+//         "type": "F",
+//         "badgesV2": {
+//           "entityBadges": {
+//             "imageBased": {},
+//             "textBased": {},
+//             "textExtendedBadges": {
+//               "badgeObject": [
+//                 {
+//                   "attributes": {
+//                     "description": "",
+//                     "fontColor": "#7E808C",
+//                     "iconId": "guiltfree/GF_Logo_android_3x",
+//                     "shortDescription": "options available"
+//                   }
+//                 }
+//               ]
+//             }
+//           }
+//         },
+//         "aggregatedDiscountInfoV3": {
+//           "header": "ITEMS",
+//           "subHeader": "AT ₹100"
+//         },
+//         "differentiatedUi": {
+//           "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//           "differentiatedUiMediaDetails": {
+//             "lottie": {},
+//             "video": {}
+//           }
+//         },
+//         "reviewsSummary": {},
+//         "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//         "restaurantOfferPresentationInfo": {},
+//         "externalRatings": {
+//           "aggregatedRating": {
+//             "rating": "--"
+//           }
+//         },
+//         "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+//       },
+//       "analytics": {
+//         "context": "seo-data-f06da16e-1808-413f-8f62-6309a814cecd"
+//       },
+//       "cta": {
+//         "link": "https://www.swiggy.com/city/delhi/nh1-bowls-highway-to-north-mayur-vihar-phase1-rest650503",
+//         "type": "WEBLINK"
+//       }
+//     },
+//     {
+//       "info": {
+//         "id": "82120",
+//         "name": "Blue Tokai Coffee Roasters",
+//         "cloudinaryImageId": "1a0521375f07155eda2f1ed263206831",
+//         "locality": "B Block",
+//         "areaName": "Udhyog Marg",
+//         "costForTwo": "₹600 for two",
+//         "cuisines": ["Cafe", "Coffee", "Beverages"],
+//         "avgRating": 4.7,
+//         "parentId": "2682",
+//         "avgRatingString": "4.7",
+//         "totalRatingsString": "2.5K+",
+//         "sla": {
+//           "deliveryTime": 24,
+//           "lastMileTravel": 4.3,
+//           "serviceability": "SERVICEABLE",
+//           "slaString": "20-25 mins",
+//           "lastMileTravelString": "4.3 km",
+//           "iconType": "ICON_TYPE_EMPTY"
+//         },
+//         "availability": {
+//           "nextCloseTime": "2025-07-05 21:00:00",
+//           "opened": true
+//         },
+//         "badges": {
+//           "imageBadges": [
+//             {
+//               "imageId": "newg.png",
+//               "description": "Gourmet"
+//             }
+//           ]
+//         },
+//         "isOpen": true,
+//         "aggregatedDiscountInfoV2": {},
+//         "type": "F",
+//         "badgesV2": {
+//           "entityBadges": {
+//             "imageBased": {
+//               "badgeObject": [
+//                 {
+//                   "attributes": {
+//                     "description": "Gourmet",
+//                     "imageId": "newg.png"
+//                   }
+//                 }
+//               ]
+//             },
+//             "textBased": {},
+//             "textExtendedBadges": {}
+//           }
+//         },
+//         "differentiatedUi": {
+//           "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//           "differentiatedUiMediaDetails": {
+//             "lottie": {},
+//             "video": {}
+//           }
+//         },
+//         "reviewsSummary": {},
+//         "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//         "restaurantOfferPresentationInfo": {},
+//         "externalRatings": {
+//           "aggregatedRating": {
+//             "rating": "--"
+//           }
+//         },
+//         "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+//       },
+//       "analytics": {
+//         "context": "seo-data-f06da16e-1808-413f-8f62-6309a814cecd"
+//       },
+//       "cta": {
+//         "link": "https://www.swiggy.com/city/delhi/blue-tokai-coffee-roasters-b-block-udhyog-marg-rest82120",
+//         "type": "WEBLINK"
+//       }
+//     },
+//     {
+//       "info": {
+//         "id": "828372",
+//         "name": "Theobroma",
+//         "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/6/23/e4d7c9d3-bbb2-432e-bdf4-5e549f5ff6ee_828372.JPG",
+//         "locality": "Nextra Plot No 48",
+//         "areaName": "District Centre",
+//         "costForTwo": "₹300 for two",
+//         "cuisines": ["Bakery", "Desserts", "Beverages"],
+//         "avgRating": 4.6,
+//         "parentId": "1040",
+//         "avgRatingString": "4.6",
+//         "totalRatingsString": "657",
+//         "sla": {
+//           "deliveryTime": 21,
+//           "lastMileTravel": 3.5,
+//           "serviceability": "SERVICEABLE",
+//           "slaString": "20-25 mins",
+//           "lastMileTravelString": "3.5 km",
+//           "iconType": "ICON_TYPE_EMPTY"
+//         },
+//         "availability": {
+//           "nextCloseTime": "2025-07-06 00:00:00",
+//           "opened": true
+//         },
+//         "badges": {
+//           "imageBadges": [
+//             {
+//               "imageId": "Rxawards/_CATEGORY-Desserts.png",
+//               "description": "Delivery!"
+//             },
+//             {
+//               "imageId": "newg.png",
+//               "description": "Gourmet"
+//             }
+//           ]
+//         },
+//         "isOpen": true,
+//         "type": "F",
+//         "badgesV2": {
+//           "entityBadges": {
+//             "imageBased": {
+//               "badgeObject": [
+//                 {
+//                   "attributes": {
+//                     "description": "Delivery!",
+//                     "imageId": "Rxawards/_CATEGORY-Desserts.png"
+//                   }
+//                 },
+//                 {
+//                   "attributes": {
+//                     "description": "Gourmet",
+//                     "imageId": "newg.png"
+//                   }
+//                 }
+//               ]
+//             },
+//             "textBased": {},
+//             "textExtendedBadges": {}
+//           }
+//         },
+//         "aggregatedDiscountInfoV3": {
+//           "header": "15% OFF",
+//           "discountTag": "FLAT DEAL"
+//         },
+//         "differentiatedUi": {
+//           "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//           "differentiatedUiMediaDetails": {
+//             "lottie": {},
+//             "video": {}
+//           }
+//         },
+//         "reviewsSummary": {},
+//         "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//         "restaurantOfferPresentationInfo": {},
+//         "externalRatings": {
+//           "aggregatedRating": {
+//             "rating": "--"
+//           }
+//         },
+//         "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+//       },
+//       "analytics": {
+//         "context": "seo-data-f06da16e-1808-413f-8f62-6309a814cecd"
+//       },
+//       "cta": {
+//         "link": "https://www.swiggy.com/city/delhi/theobroma-nextra-plot-no-48-district-centre-rest828372",
+//         "type": "WEBLINK"
+//       }
+//     },
+//     {
+//       "info": {
+//         "id": "15671",
+//         "name": "Behrouz Biryani",
+//         "cloudinaryImageId": "a4ffed13eb197c6df43dfe1c756560e5",
+//         "locality": "R Block",
+//         "areaName": "Shakarpur",
+//         "costForTwo": "₹500 for two",
+//         "cuisines": [
+//           "Biryani",
+//           "North Indian",
+//           "Kebabs",
+//           "Mughlai",
+//           "Beverages",
+//           "Desserts"
+//         ],
+//         "avgRating": 4.4,
+//         "parentId": "1803",
+//         "avgRatingString": "4.4",
+//         "totalRatingsString": "21K+",
+//         "sla": {
+//           "deliveryTime": 28,
+//           "lastMileTravel": 4.3,
+//           "serviceability": "SERVICEABLE",
+//           "slaString": "25-30 mins",
+//           "lastMileTravelString": "4.3 km",
+//           "iconType": "ICON_TYPE_EMPTY"
+//         },
+//         "availability": {
+//           "nextCloseTime": "2025-07-05 23:59:00",
+//           "opened": true
+//         },
+//         "badges": {
+//           "imageBadges": [
+//             {
+//               "imageId": "newg.png",
+//               "description": "Gourmet"
+//             }
+//           ]
+//         },
+//         "isOpen": true,
+//         "type": "F",
+//         "badgesV2": {
+//           "entityBadges": {
+//             "imageBased": {
+//               "badgeObject": [
+//                 {
+//                   "attributes": {
+//                     "description": "Gourmet",
+//                     "imageId": "newg.png"
+//                   }
+//                 }
+//               ]
+//             },
+//             "textBased": {},
+//             "textExtendedBadges": {}
+//           }
+//         },
+//         "aggregatedDiscountInfoV3": {
+//           "header": "ITEMS",
+//           "subHeader": "AT ₹99"
+//         },
+//         "differentiatedUi": {
+//           "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+//           "differentiatedUiMediaDetails": {
+//             "lottie": {},
+//             "video": {}
+//           }
+//         },
+//         "reviewsSummary": {},
+//         "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+//         "restaurantOfferPresentationInfo": {},
+//         "externalRatings": {
+//           "aggregatedRating": {
+//             "rating": "--"
+//           }
+//         }
+//       }
+//     }
